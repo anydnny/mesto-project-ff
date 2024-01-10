3659bafefd254b7177bc825f6f0a78645cb25b0e
Daniil Glushakov <anydnny@gmail.com>
2024-01-10 08:42:47 +0300

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardList = document.querySelector('.places__list');
+
+function addCard(item) {
+
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  const cardDeleteBtn = cardElement.querySelector('.card__delete-button');
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  function removeCard() {
+    cardElement.remove()
+  }
+
+  cardImage.src = `${item.link}`;
+  cardImage.alt = `На фото ${item.name}`;
+  cardTitle.textContent = `${item.name}`;
+  cardDeleteBtn.addEventListener('click', removeCard)
+
+  cardList.append(cardElement);
+}
+
+function addStarterCards() {
+  initialCards.forEach(item => addCard(item))
+}
+
+addStarterCards()
